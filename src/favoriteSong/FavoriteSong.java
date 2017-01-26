fc417f5023db968b49cd50c18e4289811ebcff7e
Frick112 <sf137412@students.davenport.k12.ia.us>
2017-01-26 16:59:34 -0600

    asdf

---
+++ src/favoriteSong/FavoriteSong.java
@@ -0,0 +1,14 @@
+package favoriteSong;
+
+public class FavoriteSong 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Little bit of humble, little bit of cautious");
+		System.out.println("Somewhere between like Rocky and Cosby");
+		System.out.println("Sweater game nope nope y’all can’t copy");
+		System.out.println("Yup, Bad, moonwalking");
+	}
+
+}
